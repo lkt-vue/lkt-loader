3c6e31a6046874edc234892986663ff6e2ddacae
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2024-02-27 11:05:19 +0100

    Test module declaration

---
+++ dist/build.d.ts
@@ -0,0 +1,6 @@
+declare namespace S {
+    function install(t: any): void;
+}
+declare function $(t: any): void;
+declare function x(t: any): void;
+export { S as default, $ as setCustomLoader, x as setCustomSpinner };
